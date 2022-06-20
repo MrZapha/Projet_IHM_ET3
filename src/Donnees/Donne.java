cbe89a2595354217eab1efd25c08c290653735b6
Skyman28 <107101306+Skyman28@users.noreply.github.com>
2022-06-20 14:31:07 +0200

    donnee

--- src/Donnees/Donne.java
+++ src/Donnees/Donne.java
@@ -1,5 +1,59 @@
 package Donnees;
 
-public class Donne {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
+import com.ludovic.vimont.GeoHashHelper;
+import com.ludovic.vimont.Location;
+
+public class Donne {
+	private int s=0;
+	private ArrayList<Enregistrement> list;
+	
+	public Donne() {
+		list=new ArrayList<Enregistrement>();
+	}
+	
+	public void add_Enregistrement(Enregistrement e) {
+		list.add(e);
+	}
+	
+	int nb_signalement_region(String nom,String region) {
+		s=0;
+		Consumer<Enregistrement> conter_espece = new Consumer<Enregistrement>() {
+			@Override
+			public void accept(Enregistrement t) {
+				if (t.get_nom().equals(nom) && t.get_region().equals(region)) {
+					s++;
+				}
+				
+			};
+        };
+		
+		list.forEach(conter_espece);
+		return s;
+	}
+	
+	public int nb_signalement_region(String nom,Double longitude,Double latitude) {
+		return nb_signalement_region(nom,GeoHashHelper.getGeohash(new Location("",longitude,latitude)));
+	}
+	
+	public static Donne initialize() {
+		Donne d=new Donne();
+		
+		return d;
+	}
+	
+	public static void main(String args[]) {
+		Donne d=new Donne();
+		Enregistrement e=new Enregistrement(20.0,30.0,"Dolphin",LocalDate.now());
+		d.add_Enregistrement(e);
+		Enregistrement f=new Enregistrement(GeoHashHelper.getGeohash(new Location("dolphin",40,30)),"Dolphin",LocalDate.now());
+		d.add_Enregistrement(f);
+		Enregistrement g=new Enregistrement(GeoHashHelper.getGeohash(new Location("dolphin",20,30)),"Dolphin",LocalDate.now());
+		d.add_Enregistrement(g);
+		System.out.println(d.nb_signalement_region("Dolphin",20.0,30.0));
+		System.out.println(d.list);
+	}
 }
--- src/Donnees/Enregistrement.java
+++ src/Donnees/Enregistrement.java
@@ -1,20 +1,23 @@
 package Donnees;
 
-import java.util.Date;
+import java.time.LocalDate;
 import com.ludovic.vimont.GeoHashHelper;
 import com.ludovic.vimont.Location;
 
 public class Enregistrement {
 	private String region;
 	private String nom;
-	private Date date_enregistrement;
+	private LocalDate date_enregistrement;
 	
-	public Enregistrement(String region,String nom, Date date_enregistrement) {
+	Enregistrement(String region,String nom, LocalDate date_enregistrement) {
 		this.region=region;
 		this.nom=nom;
 		this.date_enregistrement=date_enregistrement;
 	}
 	
+	public Enregistrement(Double latitude,Double longitude,String nom, LocalDate date_enregistrement) {
+		this(GeoHashHelper.getGeohash(new Location("",20,30)),nom,date_enregistrement);
+	}
 	
 	public String get_region() {
 		return region;
@@ -24,19 +27,12 @@ public class Enregistrement {
 		return nom;
 	}
 	
-	public Date get_date() {
+	public LocalDate get_date() {
 		return date_enregistrement;
 	}
 	
 	@Override
 	public String toString() {
-		return "C'est un "+nom+", qui a été enregistré le "+date_enregistrement.toString()+", dans la region: "+GeoHashHelper.getLocation(region)+".";
-	}
-	
-	public static void main(String[] args) {
-		Location loc=new Location("selectedGeoHash",20,60);
-		Enregistrement e=new Enregistrement(GeoHashHelper.getGeohash(loc),"dauphin",new Date());
-		System.out.println(e);
-		System.out.print(e.get_region());
+		return "C'est un "+nom+", qui a été enregistré le "+date_enregistrement.toString()+", dans la region: "+GeoHashHelper.getLocation(region)+"\n";
 	}
 }
