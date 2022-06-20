dd1d31fd1d3531203ec7915c535eb1fe2cb836d3
MrZapha <73909093+MrZapha@users.noreply.github.com>
2022-06-20 15:06:07 +0200

    gg

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,11 @@
 module Projet_IHM_ET3 {
 	requires javafx.controls;
+<<<<<<< Updated upstream
 	requires java.net.http;
 	requires org.json;
+=======
+	requires javafx.fxml;
+>>>>>>> Stashed changes
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
